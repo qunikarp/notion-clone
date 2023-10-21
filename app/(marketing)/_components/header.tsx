8037cc7d14f5d1fe0018805402caf0079f5bdef4
qunikarp <80711470+qunikarp@users.noreply.github.com>
2023-10-21 22:04:51 +0200

    init

---
+++ app/(marketing)/_components/header.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export default function Header() {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl">
+        Your notes and tasks, structured. Welcome to <br />
+        <span className="font-bold">Hikki</span>
+      </h1>
+      <h2 className="text-base sm:text-xl md:text-2xl font-medium">
+        Hikki is the only tool you need to organize your notes and tasks. It
+        combines the simplicity of a note-taking app with the power of a project
+        management tool.
+      </h2>
+      <Button>
+        Join Hikki Beta
+        <ArrowRight className="ml-2 w-4 h-4" />
+      </Button>
+    </div>
+  );
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./_components/header";
+
+function MarketingPage() {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default MarketingPage;
